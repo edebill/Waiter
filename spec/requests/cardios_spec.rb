1dde273ac5e4cbbfece0d23e7d6827280f9eba96
Erik DeBill <erik@debill.org>
2010-06-06 13:50:24 -0500

    remove test files from older models/controllers

--- spec/requests/cardios_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Cardios" do
-  describe "GET /cardios" do
-    it "works! (now write some real specs)" do
-      get cardios_path
-    end
-  end
-end
--- spec/requests/weights_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Weights" do
-  describe "GET /weights" do
-    it "works! (now write some real specs)" do
-      get weights_path
-    end
-  end
-end
