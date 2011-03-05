42f2c670e477f5384205042ed204e41ff41bab96
Erik DeBill <erik@debill.org>
2011-03-05 08:45:04 -0600

    make it work with 'rails s' or heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Waiter::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   config.action_mailer.default_url_options = { :host => 'waiter.ediblechaos.net' }
 
