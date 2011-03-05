7a75a0f43b24711e83bf5a2bb5d721bdb884673e
Erik DeBill <erik@debill.org>
2011-03-05 08:44:07 -0600

    Use some better footer control to avoid overlaying content that grows, center the tab section horizontally, make copyright year auto-update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def copyright_years(start)
+    now_year = Time.now.year
+    if start == now_year
+      now_year
+    else
+      "#{start}&mdash;#{now_year}".html_safe
+    end
+  end
 end
