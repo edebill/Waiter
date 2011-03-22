e87fc068c4cf8565308b8c076fde6b416770bc09
Erik DeBill <erik@debill.org>
2011-03-22 07:37:09 -0500

    suddenly, it broke because of memcache - this gets it working

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
-CACHE = MemCache.new(:namespace => 'waiter')
-CACHE.servers = 'localhost:11211'
+CACHE = Dalli::Client.new('localhost:11211')
 
 #Rails.application.config.session_store :mem_cache_store
-
-Rails.application.config.session_store :mem_cache_store, {
-  :cache => CACHE,
+require 'action_dispatch/middleware/session/dalli_store'
+Rails.application.config.session_store :dalli_store, {
+  :memcache_server => ['127.0.0.1:11211'],
+  :namespace => 'waiter',
   :session_key => '_waiter_session',
   :secret => 'kWleS2hzWDvvEHYj1bK5ZYo8as098asdf3098sfkhjzv7KSHVB',
   :expire_after => 172800
