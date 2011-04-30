78dfe57ef9c8f8796d6fed6dbffc2fd36b5613bf
Erik DeBill <edebill@moxiesoft.com>
2011-04-30 11:08:56 -0500

    looks like newer devise needs an :encryptable option to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable,
+         :encryptable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
