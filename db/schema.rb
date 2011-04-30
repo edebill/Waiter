d40d02247f65af3eaa83565e60a7a46b9973935f
Erik DeBill <erik@debill.org>
2011-04-30 15:32:16 +0000

    local schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101031162905) do
+ActiveRecord::Schema.define(:version => 20110329150416) do
 
   create_table "biometric_data", :force => true do |t|
     t.integer  "biometrics_id"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(:version => 20101031162905) do
 
   add_index "events", ["event_date"], :name => "index_events_on_event_date"
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
