8b49d9e540e1a73d5e27deb7b21f4fd292938500
Erik DeBill <edebill@moxiesoft.com>
2011-05-09 07:38:28 -0500

    switch to database sessions - too many problems connecting to memcache

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,18 +1,18 @@
 # Be sure to restart your server when you modify this file.
-CACHE = Dalli::Client.new('localhost:11211')
+#CACHE = Dalli::Client.new('localhost:11211')
 
 #Rails.application.config.session_store :mem_cache_store
-require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store :dalli_store, {
-  :memcache_server => ['127.0.0.1:11211'],
-  :namespace => 'waiter',
-  :session_key => '_waiter_session',
-  :secret => 'kWleS2hzWDvvEHYj1bK5ZYo8as098asdf3098sfkhjzv7KSHVB',
-  :expire_after => 172800
-}
+#require 'action_dispatch/middleware/session/dalli_store'
+#Rails.application.config.session_store :dalli_store, {
+#  :memcache_server => ['127.0.0.1:11211'],
+#  :namespace => 'waiter',
+#  :session_key => '_waiter_session',
+#  :secret => 'kWleS2hzWDvvEHYj1bK5ZYo8as098asdf3098sfkhjzv7KSHVB',
+#  :expire_after => 172800
+#}
 
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Rails.application.config.session_store :active_record_store
+Rails.application.config.session_store :active_record_store
